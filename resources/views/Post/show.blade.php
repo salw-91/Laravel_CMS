fd3799bcd37073a9ba7dfabad1340352b5059f3d
Salwan-Aldulaimi <salwan@holygrow.nl>
2020-01-06 16:27:47 +0100

    Show post work good.

--- resources/views/Post/show.blade.php
+++ resources/views/Post/show.blade.php
@@ -6,57 +6,30 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">Show pots.</div>
-
                     <div class="card-body">
                         <table class="col-md-12 table float-right">
                             <thead>
                             <tr>
-
                                 <th>Title</th>
                                 <th>Content</th>
                                 <th>Category</th>
                                 <th>To</th>
                                 <th>From</th>
                                 <th>Tag</th>
-
                             </tr>
                             </thead>
                             <tbody>
+                            <th>{{$post->title}}</th>
+                            <th>{{$post->content}}</th>
+                            <th>{{$post->category->name}}</th>
+                            <th>{{$post->fromuser->name}}</th>
+                            <th>{{$post->touser->name}}</th>
 
-                                        <th>{{$post->title}}</th>
-                                        <th>{{$post->content}}</th>
-                                        <th>{{$post->category->name}}</th>
-                                        <th>{{$post->fromuser->name}}</th>
-                                        <th>{{$post->touser->name}}</th>
-
-                                        <th>
-                                            @foreach($post->tags as $tag)
-                                                {{$tag->name}}
-                                            @endforeach
-                                        </th>
-
-
-                                                {{-- <form action="{{ route('post.show', ['id'=>$post->id]) }}">
-                                                    <button type="submit" class="btn btn-warning">
-                                                        <i class="far fa-eye"></i>
-                                                    </button>
-                                                </form>
-                                            </th>
-                                            <th>
-                                                <form action="{{ route('post.edit', ['id'=>$post->id]) }}">
-                                                    <button type="submit" class="btn btn-info">
-                                                        <i class="fas fa-pencil-alt"></i>
-                                                    </button>
-                                                </form>
-                                            </th>
-                                            <th>
-                                                <form action="{{ route('post.destroy', ['id'=>$post->id]) }}">
-                                                    <button type="submit" class="btn btn-danger">
-                                                        <i class="far fa-trash-alt"></i>
-                                                    </button>
-                                                </form> --}}
-
-
+                            <th>
+                                @foreach($post->tags as $tag)
+                                    {{$tag->name}}
+                                @endforeach
+                            </th>
                             </tbody>
                         </table>
                     </div>
